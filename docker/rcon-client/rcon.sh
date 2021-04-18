23527703658265e55f77998980c1165251efaace
René Gastmeier <rene@coderitter.io>
2021-04-18 12:24:27 +0200

    Fix missing interactive mode in rcon script and add comment on how to access localhost

--- docker/rcon-client/rcon.sh
+++ docker/rcon-client/rcon.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-# use like this ./rcon 127.0.0.1:27960 rcon_password
+# use like this ./rcon.sh 127.0.0.1:27960 rcon_password
+
+# since this command runs in a Docker container which is its own machine
+# the localhost will point to that Docker container and not to your computer
+# hosting the Docker container. If you want to access a Quake Live server which
+# is running on your machine you have to use the address host.docker.internal
+# instead of localhost.
+
 python2 zmq_rcon.py --host=tcp://$1 --password=$2
--- rcon.sh
+++ rcon.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 # use like this ./rcon.sh 127.0.0.1:27960 rcon_password
-docker run quakeliveserverstandards/rcon-client $1 $2
+
+# since this command runs in a Docker container which is its own machine
+# the localhost will point to that Docker container and not to your computer
+# hosting the Docker container. If you want to access a Quake Live server which
+# is running on your machine you have to use the address host.docker.internal
+# instead of localhost.
+
+docker run -i --add-host host.docker.internal:host-gateway quakeliveserverstandards/rcon-client $1 $2
