26f056f2022f897422ec087725f1f67fad5420a6
René Gastmeier <rene@coderitter.io>
2021-04-19 17:34:52 +0200

    Fix downloading of workshop items

--- docker/download-workshop/download.sh
+++ docker/download-workshop/download.sh
@@ -4,3 +4,4 @@
 
 set -x
 /home/steam/steamcmd/steamcmd.sh +login anonymous +workshop_download_item 282440 $1 validate +quit
+mv /home/steam/Steam/steamapps/workshop/content/282440/$1 /home/steam/items
--- workshop/download.sh
+++ workshop/download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # use like this ./download.sh 691078677
-docker run --rm --volume items:/home/steam/Steam/steamapps/workshop/content/282440 quakeliveserverstandards/download-workshop $1
+docker run --rm --volume $PWD/items:/home/steam/items quakeliveserverstandards/download-workshop $1
