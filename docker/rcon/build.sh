a55f701f7a9edc4534e98f32fd57859918424efd
René Gastmeier <rene@coderitter.io>
2021-05-02 16:13:12 +0200

    Restructure script directory and use ql-console as standard ql server client

--- docker/rcon/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ../..
-docker build -t quakeliveserverstandards/rcon -f docker/rcon/Dockerfile .
--- servers/connect.sh
+++ servers/connect.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# use like this ./rcon.sh 127.0.0.1:27960 rcon_password
+# use like this
+# ./connect.sh 127.0.0.1 --rcon-port 28960 --rcon-password rconadmin --stats-port 27960 --stats-password statsadmin
+# further instructions: https://github.com/quakelive-server-standards/ql-console#readme
 
 # since this command runs in a Docker container which is its own machine
 # the localhost will point to that Docker container and not to your computer
@@ -8,4 +10,4 @@
 # is running on your machine you have to use the address host.docker.internal
 # instead of localhost.
 
-docker run --rm -i --add-host host.docker.internal:host-gateway quakeliveserverstandards/rcon $1 $2
+docker run --rm -ti --add-host host.docker.internal:host-gateway quakeliveserverstandards/ql-console $*
