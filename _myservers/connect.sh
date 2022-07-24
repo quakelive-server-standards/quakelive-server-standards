ec1c96ba60b5b22142d4bee86c6887ba46076fab
René Gastmeier <rene@coderitter.io>
2022-07-25 00:28:39 +0200

    Fix letter casing in the beginning of sentences

--- _myservers/connect.sh
+++ _myservers/connect.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# use like this
+# Use like this:
 # ./connect.sh 127.0.0.1 --rcon-port 28960 --rcon-password rconadmin --stats-port 27960 --stats-password statsadmin
-# further instructions: https://github.com/quakelive-server-standards/ql-console#readme
+# Further instructions: https://github.com/quakelive-server-standards/ql-console#readme
 
-# since this command runs in a Docker container which is its own machine
+# Since this command runs in a Docker container which is its own machine
 # the localhost will point to that Docker container and not to your computer
 # hosting the Docker container. If you want to access a local Quake Live server which
 # is running on your machine you have to use the address host.docker.internal
